ec2c8f93665ecfa528751b9f28af5045b609ad09
Chris Porter <cap8095@gmail.com>
2022-04-07 18:08:34 -0400

    early stages of JS work

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var getCityWeather = function () {};
+var apiKey = "0791b2dc9af57326d63bc40bd08ec24f";
+var getCityWeather = function (cityName) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+  fetch(apiUrl);
+};
 getCityWeather();
